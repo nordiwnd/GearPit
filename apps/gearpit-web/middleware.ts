7e939430ddd2221c6f9c3afb6c31d4c5fb0dc289
nordwind <netgsllap@gmail.com>
2026-02-07 12:15:18 +0900

    chore: Update web application request handling middleware.

--- apps/gearpit-web/middleware.ts
+++ apps/gearpit-web/middleware.ts
@@ -4,13 +4,14 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   // /api/ から始まるリクエストを検知
   if (request.nextUrl.pathname.startsWith('/api/')) {
-    // 実行時に環境変数を読み込む (なければ localhost)
-    const backendUrl = process.env.BACKEND_URL || 'http://localhost:8080';
-    
+    // 実行時に環境変数を読み込む (なければ K8s Service)
+    // NOTE: deployment.yamlで INTERNAL_API_URL が設定されている
+    const backendUrl = process.env.INTERNAL_API_URL || 'http://gearpit-app-svc/api/v1';
+
     // バックエンドのURLを構築
     // 例: http://gearpit-app-svc...:80/api/v1/gears
     const targetUrl = new URL(request.nextUrl.pathname + request.nextUrl.search, backendUrl);
-    
+
     // リライト（プロキシ）実行
     return NextResponse.rewrite(targetUrl);
   }
